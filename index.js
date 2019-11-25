d645887bf79c8908fae73a50675455ec64a1ff82
Trevor Watson <watsontrevor10@gmail.com>
2019-11-25 16:32:57 -0700

    got win incrementer and cleaned up interface (a bit)

--- index.js
+++ index.js
@@ -3,44 +3,58 @@ var compChoiceOptions = ["rock", "paper", "scissors"]
 var compChoice = ""
 var userChoice = ""
 var whoWon = ""
+var userWin = 0
+var compWin = 0
+var tiedGame = 0
 
 for(var i = 0; i < choices.length; i++) {
   choices[i].addEventListener("click", function() {
     userChoice = this.id
     document.getElementById("userDecision").innerHTML = "You Chose: " + userChoice
-
+    
     compChoice = compChoiceOptions[Math.floor(Math.random() * compChoiceOptions.length)];
     document.getElementById("compDecision").innerHTML = "Computer Chose: " + compChoice;
     
     compare(userChoice, compChoice)
+
+    userWin = document.getElementById("userWins").innerHTML = userWin
+    compWin = document.getElementById("compWins").innerHTML = compWin
+    tiedGame = document.getElementById("tie").innerHTML = tiedGame
   })
 }
 
 var compare = function(userChoice, compChoice) {
   if (userChoice === compChoice) {
     document.getElementById("whoWon").innerHTML = "It was a tie"
+    tiedGame++
   } else {
     if (userChoice === "rock") {
       if (compChoice === "paper") {
         document.getElementById("whoWon").innerHTML = "Computer Wins"
+        compWin++
       } else {
         document.getElementById("whoWon").innerHTML = "You Win"
+        userWin++
       }
     };
 
     if (userChoice === "paper") {
       if (compChoice === "scissors") {
         document.getElementById("whoWon").innerHTML = "Computer Wins"
+        compWin++
       } else {
         document.getElementById("whoWon").innerHTML = "You Win"
+        userWin++
       }
     };
 
     if (userChoice === "scissors") {
       if (compChoice === "rock") {
         document.getElementById("whoWon").innerHTML = "Computer Wins"
+        compWin++
       } else {
         document.getElementById("whoWon").innerHTML = "You Win"
+        userWin++
       }
     };
 }
